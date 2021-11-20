22423b20e0a311baeacd919d23c8597b6b24ca86
Catalina Torrente <caatoba@gmail.com>
2021-11-20 12:17:14 -0500

    Crear modelos para usuarios y proyectos

---
+++ backend/models/project/projectModel.js
@@ -0,0 +1,95 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const projectSchema = new Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    objetivosGenerales: {
+        type: String,
+        required: true
+    },
+    objetivosEspecificos: {
+        type: Array,
+        required: true
+    },
+    presupuesto: {
+      type: Number,
+      required: true,
+    },
+    fechaInicio: {
+      type: Date,
+      default: new Date()
+    },
+    fechaFin: {
+      type: Date,
+    },
+    lider:{
+      type:String,
+      required: true
+
+    }, 
+    idLider: {
+      type:String,
+      required: true,
+    },
+    estado: {
+      type: String,
+      enum: ['Activo', 'Inactivo'],
+      default: 'Inactivo',
+    },
+    fase: {
+      type: String,
+      enum: ['Iniciado', 'En_Desarrollo', 'Terminado', null],
+      default: null,
+    },
+    inscripcion: [{
+        idInscripcion: {
+            type: Schema.Types.ObjectId,
+            required: true,            
+        },
+        idEstudiante: {
+            type: String,
+            required: true,
+        },
+        estado: {
+            type: String,
+            enum: ['Aceptado', 'Rechazado', 'Pendiente'],
+            default: 'Pendiente',
+            required: true,
+        },
+        fechaDeIngreso: {
+            type: Date,
+        },
+        fechaDeEgreso: {
+            type: Date,
+        }
+    }],
+    avance: [{
+        idAvance: {
+            type: Schema.Types.ObjectId,
+            required: true
+            },
+            fecha: {
+                type: Date,
+                required: true,
+            },
+            descripcion: {
+                type: String,
+                required: true,
+            },
+            observacionesDelLider: {
+                type: Array,
+                required: true,
+            }
+        
+    }]
+   
+  },
+);
+
+const ProjectModel = model('Proyecto', projectSchema);
+
+export { ProjectModel };
