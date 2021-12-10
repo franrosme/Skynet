e5d895b4fa8bea41439919b353aa275de6254c37
Catalina Torrente <caatoba@gmail.com>
2021-12-10 11:06:02 -0500

    agregar estilos y actualizar editar  proyecto

--- frontend/src/pages/proyectos/editar.jsx
+++ frontend/src/pages/proyectos/editar.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
-import Input from 'components/Input';
 import ButtonLoading from 'components/ButtonLoading';
 import useFormData from 'hooks/useFormData';
 import { toast } from 'react-toastify';
 import { EDITAR_PROYECTO } from 'graphql/proyectos/mutations';
 import { GET_PROYECTO } from 'graphql/proyectos/queries';
 
+
 export default function EditarProyecto (props) {
-  
-  
+  var ex = 0;
+   
   const { form, formData, updateFormData } = useFormData(null);
   const idUsuario=props._id;
   var { _id } = useParams();
@@ -30,24 +30,23 @@ export default function EditarProyecto (props) {
 
   const submitForm = (e) => {
     e.preventDefault();
-    delete formData.rol;
-
-    console.log(formData)
+  
     formData.presupuesto= parseInt(formData.presupuesto)
     const arr =[]
     
-  
-    
     for (const property in formData) {
       if(property.includes("E")){
+        console.log(`${property}: ${formData[property]}`);
         arr.push(formData[property])
         delete formData[property];
-        //console.log(`${property}: ${formData[property]}`);
+        
       }
      
     }
+    console.log(formData)
+    console.log("----"+arr)
     formData.objetivosEspecificos= arr;
-    console.log(arr)
+   
     const campos=formData
     EditarProyecto({
       variables: { idUsuario, idProyecto, campos},
@@ -56,78 +55,138 @@ export default function EditarProyecto (props) {
 
   useEffect(() => {
     if (mutationData) {
-      toast.success('Usuario modificado correctamente');
+      toast.success('Proyecto actualizado correctamente');
+      setTimeout( function() { window.location.href = `/proyecto/${idProyecto}`; }, 5000 );
     }
-  }, [mutationData]);
+  }, [idProyecto, mutationData]);
 
   useEffect(() => {
     if (mutationError) {
-      toast.error('Error modificando el usuario');
+      toast.error('Error actualizando el proyecto');
+      setTimeout( function() { window.location.href = `/proyecto/${idProyecto}`; }, 5000 );
+      
     }
 
     if (queryError) {
-      toast.error('Error consultando el usuario');
+      toast.error('Error consultando el proyecto');
+      setTimeout( function() { window.location.href = `/proyecto/${idProyecto}`; }, 5000 );
     }
-  }, [queryError, mutationError]);
+  }, [queryError, mutationError, idProyecto]);
 
   if (queryLoading) return <div>Cargando....</div>;
-console.log("query data: "+queryData)
   return (
+    
     <div className='flew flex-col w-full h-full items-center justify-center p-10'>
       <Link to='/proyectos'>
         <i className='fas fa-arrow-left text-gray-600 cursor-pointer font-bold text-xl hover:text-gray-900' />
       </Link>
-      <h1 className='m-4 text-3xl text-gray-800 font-bold text-center'>Editar Proyecto</h1>
-      <form
+      
+    <div class="h-96"></div>
+    <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
+        <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
+            <p class="text-3xl font-bold leading-7 text-center">Editar Proyecto</p>
+            <form
         onSubmit={submitForm}
         onChange={updateFormData}
         ref={form}
-        className='flex flex-col items-center justify-center'
-      >
-        <Input
-          label='Nombre Proyecto:'
-          type='text'
-          name='nombre'
-          defaultValue={queryData.VerProyecto.nombre}
-          required={true}
-        />
-     
-        <Input as="textarea"
-          label='Objetivo General:'
-          type='text'
-          name='objetivosGenerales'
-          defaultValue={queryData.VerProyecto.objetivosGenerales}
-          required={true}
-        />
-        <h1>Objetivos Especificos:</h1>
-        {queryData.VerProyecto.objetivosEspecificos.map((u, index) => {
+        >
+                <div class="md:flex items-center mt-8">
+                    <div class="w-full flex flex-col">
+                        <label class="font-semibold leading-none">Nombre del Proyecto:</label>
+                        <input 
+                        type='text'
+                        name='nombre'
+                        defaultValue={queryData.VerProyecto.nombre}
+                        required={true} 
+                        className="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+                    </div>
+                    
+                </div>
+                <div class="md:flex items-center mt-12">
+                    <div class="w-full md:w-1/2 flex flex-col">
+                        <label class="font-semibold leading-none">Presupuesto:</label>
+                        <input 
+                         type='number'
+                         name='presupuesto'
+                         defaultValue={queryData.VerProyecto.presupuesto}
+                         required={true} 
+                         className="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" />
+                    </div>
+                </div>
+                <div>
+                    <div class="w-full flex flex-col mt-8">
+                        <label class="font-semibold leading-none">Objetivo General:</label>
+                        <textarea
+                        name='objetivosGenerales'
+                        defaultValue={queryData.VerProyecto.objetivosGenerales}
+                        required={true}
+                        type="text"
+                        className="h-20 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"></textarea>
+                    </div>
+                </div>
+                <div>
+                    <div class="w-full flex flex-col mt-8">
+                        <label class="font-semibold leading-none">Objetivos Específicos:</label>
+                      
+                        { queryData.VerProyecto.objetivosEspecificos.length>0 ?(
+                        queryData.VerProyecto.objetivosEspecificos.map((u, index) => {
+          ex= index;
           return(
-          <Input as="textarea"
-          type='text'
-          name={`E${index}`}
-          defaultValue={u}
-          required={true}
-        />);
+          <div key={u.index}>
+            <input
+            type='text'
+            name={`E${index}`}
+            defaultValue={u}
+            required={true} 
+            ex={index}
+            className= "h-auto text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+            
+</div>
+        );
 
-        })
+        })):(<input
+          type='text'
+          name={`E0`}
+          required={true} 
+          className= "h-auto text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"/>)
+       
 
         }
+        <div id="inputDiv"></div>
+        <button onClick={(e) => {
+          
+            e.preventDefault();
+            ex=ex+1;
+            var parent = document.getElementById("inputDiv");
+
+            var input = document.createElement("input");
+            input.type= 'text'
+            input.name = `E${ex}`; 
+            input.className = "h-auto text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200"
+            parent.appendChild(input);
+            
+           
+          }}> <i className='fas fa-plus-circle text-blue-600 hover:text-blue-400 cursor-pointer' /></button>
         
-         <Input
-          label='Presupuesto:'
-          type='number'
-          name='presupuesto'
-          defaultValue={queryData.VerProyecto.presupuesto}
-          required={true}
-        />
-        
-        <ButtonLoading
-          disabled={Object.keys(formData).length === 0}
+                    
+                    
+                    </div>
+                </div>
+                <div class="flex items-center justify-center w-full">
+                <ButtonLoading
+         
           loading={mutationLoading}
           text='Confirmar'
         />
-      </form>
+               
+                </div>
+            </form>
+        </div>
     </div>
+</div>
+      
+      
+      
   );
 };
 
